39ec1e08e10484351603400ce11b9908a418f039
James Laska <1051173+jlaska@users.noreply.github.com>
2026-08-18 11:54:03 -0400

    feat(jira): Add JIRA mcp_server api token

--- zsh/.oh-my-zsh/custom/jira.zsh
+++ zsh/.oh-my-zsh/custom/jira.zsh
@@ -1,3 +1,4 @@
 # To update the token in the future, just run:
 # $ security add-generic-password -U -s "jira-api-token" -a "$USER" -w "<new-token>"
 export JIRA_API_TOKEN="$(security find-generic-password -s "jira-api-token" -a "$USER" -w 2>/dev/null)"
+export JIRA_MCP_API_TOKEN="$(security find-generic-password -s "mcp-atlassian-api-token" -a "$USER" -w 2>/dev/null)"
